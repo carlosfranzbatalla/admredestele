b148924b03a6c8f7f0b3d5051758d8d694025321
cfbatalla <carlosfranzbatalla@gmail.com>
2021-08-02 18:53:57 -0400

    se arregla fucnion obtienePuertaDeEnlace

--- monitor.sh
+++ monitor.sh
@@ -2,17 +2,18 @@
 vFechaHoy=$(date +%Y%m%d-%H:%M:%S)
 vArchivoCsv='/home/carlosfranz/Documents/MyCode/telematica/sandbox/amonitor.csv'
 vDominios=('www.yahoo.com' 'sed.ucla.edu.ve' 'www.ucla.edu.ve')
+vDns='8.8.8.8'
 
 creaCsv(){
 echo $1','$2','$3','$4','$5','$6 >> ${vArchivoCsv}
 }
 
 obtienePuertaDeEnlace(){
-   gateway=$(ip route show 0.0.0.0/0 | awk '{print $3}')
+   gateway=$(ip route get $vDns | awk '{print $3}')
 }
 
 verificaOperatividad(){    
-    taman=$(ping -qc1 $1 2>&1 | awk -F'/' 'END{ print (/^rtt/? "SI "$5" ms":"NO") }')
+    vResult=$(ping -qc1 $1 2>&1 | awk -F'/' 'END{ print (/^rtt/? "SI "$5" ms":"NO") }')
 }
 
 creaLineaCsv(){
@@ -20,7 +21,7 @@ creaLineaCsv(){
     for ((vIndice = 0; vIndice < ${vNroDeDominios}; vIndice++)); do                
         verificaOperatividad "${vDominios[$vIndice]}"
         obtienePuertaDeEnlace        
-        creaCsv $HOSTNAME ${vDominios[$vIndice]} $vFechaHoy $gateway $taman:0:3 $taman:4:10 
+        creaCsv $HOSTNAME ${vDominios[$vIndice]} $vFechaHoy $gateway $vResult:0:3 $vResult:4:10 
         sleep 5
     done
 }
